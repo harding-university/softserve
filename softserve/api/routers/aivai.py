baf143cd608765bd9362c57ca3ae0410bbb83781
Richard Schneider <richard@schneiderbox.net>
2025-02-07 21:38:19 -0600

    Add more docs metadata to aivai

--- softserve/api/routers/aivai.py
+++ softserve/api/routers/aivai.py
@@ -2,12 +2,12 @@ from random import choice, randrange
 from uuid import uuid4
 
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
 
+from ..schema import *
 from ..util import engine, get_actions
 
 
-router = APIRouter()
+router = APIRouter(prefix="/aivai", tags=["aivai"])
 
 
 # TODO stub support, to be removed
@@ -25,17 +25,20 @@ def get_arbitrary_state() -> str:
     return state
 
 
-class AIvAIPlayState(BaseModel):
-    state: str
-    uuid: str
-
-
 # TODO stub
 @router.post(
-    "/aivai/play-state",
+    "/play-state",
     response_model=AIvAIPlayState,
-    tags=["aivai"],
-    summary="Get a state to play.",
+    summary="Request a state to play",
+    description="""
+Call this to get a state for which you'll calculate an action. You'll
+get a state and a uuid. Submit the resulting action, along with the
+uuid, to `/aivai/submit-action`.
+
+**Important:** This is currently only a stub that gives you an arbitrary
+state.  Development and testing against it is encouraged. In the future,
+this endpoint will require identification and authentication.
+""",
 )
 def aivai_request() -> AIvAIPlayState:
     state = get_arbitrary_state()
@@ -46,15 +49,21 @@ def aivai_request() -> AIvAIPlayState:
     return AIvAIPlayState(state=state, uuid=uuid)
 
 
-class AIvAISubmitAction(BaseModel):
-    action: str
-    uuid: str
-
-
+# TODO stub
 @router.post(
-    "/aivai/submit-action",
-    tags=["aivai"],
-    summary="Submit an action for a requested state.",
+    "/submit-action",
+    summary="Submit an action for a requested state",
+    description="""
+Call this after calling `/aivai/play-state`. See more information above.
+
+The submitted action will be checked for validity, with HTTP 422 being
+returned for invalid actions. If you believe an action is being
+improperly validated, [open an
+issue](https://github.com/harding-university/softserve/issues).
+
+**Important:** This is currently only a stub. The uuid and action will
+be validated, but everything will be forgotten once accepted.
+""",
 )
 def aivai_submit_action(s: AIvAISubmitAction):
     try:
