c562311cd2089c387679b8bb3d565f3eed8fb00e
Richard Schneider <richard@schneiderbox.net>
2025-02-21 09:47:07 -0600

    Disable Django timezone

--- project/settings.py
+++ project/settings.py
@@ -105,13 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
+USE_I18N = True
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
 
-USE_I18N = True
-
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
