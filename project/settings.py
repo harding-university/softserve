ecbbb112636dcf64918532bbfac5c82c1f5c4574
Richard Schneider <richard@schneiderbox.net>
2025-02-22 08:45:21 -0600

    Back to sqlite3

--- project/settings.py
+++ project/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": environ.get("SOFTSERVE_DB", "softserve"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
