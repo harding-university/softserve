19f8e507523db92ad6add39373791359ef1693fb
Richard Schneider <richard@schneiderbox.net>
2026-02-03 15:11:26 -0600

    Add example environment script

---
+++ env-example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export SOFTSERVE_ENGINE=/opt/game/engine/game
+
+export SOFTSERVE_STATE_REGEX=[xo\\.]{86}
+
+export SOFTSERVE_UI=/opt/game/ui/
+export SOFTSERVE_UI_PATH=/ui/
+
+export SOFTSERVE_MAX_EVENT_GAMES=1000
+
+export SOFTSERVE_MAIL_HOST="mail.example.com"
+export SOFTSERVE_MAIL_USE_TLS=true
+export SOFTSERVE_MAIL_FROM="Softserve <softserve@example.com>"
+export SOFTSERVE_ADMIN_ADDRESS="softserve-admin@example.com"
